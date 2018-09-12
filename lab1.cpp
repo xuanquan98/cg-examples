17ddf4c8a8e82838db5c01b04581c1d9b34bd15c
Giang Le <giangm9@gmail.com>
2018-09-12 10:23:26 +0700

    add comments

--- lab1.cpp
+++ lab1.cpp
@@ -1,15 +1,27 @@
 #include <GL/glut.h>
- 
+
+/**
+ * gl : các lệnh liên quan đến vẽ
+ * glut : các lệnh liên quan đến cửa sổ, màn hình ..
+ * */
+
 void displayMe(void)
 {
+    // Lệnh xóa nền
     glClear(GL_COLOR_BUFFER_BIT);
+
+
+    // Bắt đầu vẽ đa giác
     glBegin(GL_POLYGON);
+        // Các đỉnh được liệt kê sẽ tự nối với nhau và đỉnh cuối sẽ nôi với đỉnh đầu 
         glVertex3f(0.5, 0.0, 0.5);
         glVertex3f(0.5, 0.0, 0.0);
         glVertex3f(0.0, 0.5, 0.0);
         glVertex3f(0.0, 0.0, 0.5);
     glEnd();
+    // Loại bỏ các transforms
     glFlush();
+    
 }
  
 int main(int argc, char** argv)
